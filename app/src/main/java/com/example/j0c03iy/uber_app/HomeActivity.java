cfafa5db5638a2e75d3b7cd82adb6e0e7feeb55a
Jennifer Chen <jennifer.jchen@berkeley.edu>
2019-03-03 23:41:52 -0800

    Add driver and passenger buttons

--- app/src/main/java/com/example/j0c03iy/uber_app/HomeActivity.java
+++ app/src/main/java/com/example/j0c03iy/uber_app/HomeActivity.java
@@ -2,17 +2,43 @@ package com.example.j0c03iy.uber_app;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+    private Button driverButton;
+    private Button passengerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        driverButton = findViewById(R.id.driverButton);
+        passengerButton = findViewById(R.id.passengerButton);
+
+        driverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: launch driver activity
+            }
+        });
+        passengerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: launch passenger activity
+            }
+        });
+
+
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.M) {
+            checkPermission();
+        }
     }
 
 
