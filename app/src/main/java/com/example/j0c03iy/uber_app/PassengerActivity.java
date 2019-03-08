8857d668996427613fc37252dfb4cb55435d4db5
Jennifer Chen <jennifer.jchen@berkeley.edu>
2019-03-07 23:46:12 -0800

    Implement default state lifecycle methods

--- app/src/main/java/com/example/j0c03iy/uber_app/PassengerActivity.java
+++ app/src/main/java/com/example/j0c03iy/uber_app/PassengerActivity.java
@@ -2,12 +2,23 @@ package com.example.j0c03iy.uber_app;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.Marker;
+import com.pubnub.api.PubNub;
+import com.pubnub.api.callbacks.SubscribeCallback;
+import com.pubnub.api.models.consumer.PNStatus;
+import com.pubnub.api.models.consumer.pubsub.PNMessageResult;
+import com.pubnub.api.models.consumer.pubsub.PNPresenceEventResult;
 
-public class PassengerActivity extends Activity {
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class PassengerActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private SupportMapFragment mMapFragment; // MapView UI element
 
@@ -20,5 +31,78 @@ public class PassengerActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger);
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapFragment.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapFragment.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapFragment.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapFragment.onLowMemory();
+    }
+
+    /*
+        This method is called when the map is completely set up. After the map is setup,
+        the passenger will be subscribed to the driver's location channel, so their location
+        can be updated on the MapView. We use the reference to the GoogleMap object googleMap
+        to utilize any Google Maps API features.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        try {
+            mGoogleMap = googleMap;
+            mGoogleMap.setMyLocationEnabled(true);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+
+        // This code adds the listener and subscribes passenger to channel with driver's location.
+       HomeActivity.pubNub.addListener(new SubscribeCallback() {
+            @Override
+            public void status(PubNub pub, PNStatus status) {
+
+            }
+
+            @Override
+            public void message(PubNub pub, final PNMessageResult message) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+//                            TODO: Map<String, String> newLocation = JsonUtil.fromJson(message.getMessage().toString(), LinkedHashMap.class);
+//                            TODO: updateUI(newLocation);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void presence(PubNub pub, PNPresenceEventResult presence) {
+
+            }
+        });
+        HomeActivity.pubNub.subscribe()
+                .channels(Arrays.asList(Constants.PUBNUB_CHANNEL_NAME)) // subscribe to channels
+                .execute();
+
+    }
+
 
 }
