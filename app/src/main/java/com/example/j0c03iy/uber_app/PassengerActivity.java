be7d38637255a9fe6532178390282bd12d6ad9ac
Jennifer Chen <Jennifer.Chen0@walmartlabs.com>
2019-03-07 23:15:09 -0800

    Add map features in passenger activity

--- app/src/main/java/com/example/j0c03iy/uber_app/PassengerActivity.java
+++ app/src/main/java/com/example/j0c03iy/uber_app/PassengerActivity.java
@@ -3,7 +3,17 @@ package com.example.j0c03iy.uber_app;
 import android.os.Bundle;
 import android.app.Activity;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Marker;
+
 public class PassengerActivity extends Activity {
+
+    private SupportMapFragment mMapFragment; // MapView UI element
+
+    private GoogleMap mGoogleMap; // object that represents googleMap and allows us to use Google Maps API features
+
+    private Marker driverMarker; // Marker to display driver's location
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
