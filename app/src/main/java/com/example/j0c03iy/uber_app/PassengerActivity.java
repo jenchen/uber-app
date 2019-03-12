5d6d4de6ec50f78f506f527b5484f550509b9afe
Jennifer Chen <jennifer.jchen@berkeley.edu>
2019-03-11 22:40:49 -0700

    Interpolate latitude and longitude coordinate to new location

--- app/src/main/java/com/example/j0c03iy/uber_app/PassengerActivity.java
+++ app/src/main/java/com/example/j0c03iy/uber_app/PassengerActivity.java
@@ -130,6 +130,23 @@ public class PassengerActivity extends AppCompatActivity implements OnMapReadyCa
     private void animateCar(final LatLng destination) {
         final LatLng startPosition = driverMarker.getPosition();
         final LatLng endPosition = new LatLng(destination.latitude, destination.longitude);
-//        final LatLngInterpolator latLngInterpolator;
+        final LatLngInterpolator latLngInterpolator;
+    }
+
+    private interface LatLngInterpolator {
+        LatLng interpolate(float fraction, LatLng a, LatLng b);
+
+        class LinearFixed implements LatLngInterpolator {
+            @Override
+            public LatLng interpolate(float fraction, LatLng a, LatLng b) {
+                double lat = (b.latitude - a.latitude) * fraction + a.latitude;
+                double lngDelta = b.longitude - a.longitude;
+                if (Math.abs(lngDelta) > 180) {
+                    lngDelta -= Math.signum(lngDelta) * 360;
+                }
+                double lng = lngDelta * fraction + a.longitude;
+                return new LatLng(lat, lng);
+            }
+        }
     }
 }
